90e6e7da787aaa97e589739fea6f012c99f1ca96
Kennedy Rose <kennedy@kennedyrose.com>
2018-07-02 14:05:11 -0500

    Trigger

--- src/index.js
+++ src/index.js
@@ -1,3 +1,40 @@
 import fetch from 'isomorphic-fetch'
 
-console.log(`Test`)
+function findWebhooks() {
+	console.log(`Pulling webhooks from environment variables...`)
+	const webhooks = {}
+	const regUnderscore = /_/g
+	for (let i in process.env) {
+		const env = process.env[i]
+		if (env.indexOf(`WEBHOOK_`) === 0) {
+			const key = env.replace(`WEBHOOK_`)
+				.replace(regUnderscore, ' ')
+			webhooks[key] = env
+		}
+	}
+	return webhooks
+}
+
+async function triggerWebhooks(webhooks){
+	console.log(`Triggering webhooks...`)
+	const promises = []
+	for(let name in webhooks){
+		const url = webhooks[name]
+		promises.push(
+			fetch(url, { method: `post` })
+				.then(() => console.log(`Started build for ${name}...`))
+				.catch(err => { throw err })
+		)
+	}
+	try{
+		await Promise.all(promises)
+	}
+	catch(err){
+		console.error(err)
+		process.exit(1)
+	}
+	console.log(`Done!`)
+}
+
+const webhooks = findWebhooks()
+triggerWebhooks(webhooks)
